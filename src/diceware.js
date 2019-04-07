cda0465a7307079bbaf29739eae2e2e578021e55
ziedwnissi <ziedwnissi@gmail.com>
2019-04-07 22:21:14 +0200

    final Commit

--- src/diceware.js
+++ src/diceware.js
@@ -15,15 +15,16 @@ class Diceware extends Component {
     }
 
     getword(){
-
+            //génére un code de 5 chiffres chaque chiffre compris entre 1 et 6
         return Math.floor((Math.random() * 6) + 1)*10000 + Math.floor((Math.random() * 6) + 1)*1000 + Math.floor((Math.random() * 6) + 1)*100+Math.floor((Math.random() * 6) + 1)*10+ Math.floor((Math.random() * 6) + 1);
     }
 
     getPassphrase(){
+        // génere une phrase de 5 mots  extrait de fichier diceware.listword.json
         var words='';
         for(var i=0;i<5;i++)
         {
-             words = words + ' ' + file[this.getword()];
+             words = words + ' ' + file[this.getword()]; // combinaison des mots extrait de fichier pour former la passphrase
 
         }
             
@@ -37,6 +38,7 @@ class Diceware extends Component {
     return (
       <div >
           <h1>La PassPhrase:</h1>
+
         <h1>{this.state.passphrase}</h1>
 
       </div>
